7a83ebb5a75fe39a569e6588297c1e5ed3c06ffc
REIG Julien <reigj@hostname.iut2.upmf-grenoble.fr>
2019-10-15 09:59:09 +0200

    img

---
+++ data/create.sql
@@ -0,0 +1,16 @@
+CREATE TABLE article (
+	ref INTEGER PRIMARY KEY,
+	libelle TEXT,
+	categorie INTEGER,
+	prix REAL,
+	image TEXT,
+  description TEXT,
+	FOREIGN KEY(categorie) REFERENCES categorie(id)
+);
+
+CREATE TABLE categorie (
+	id INTEGER PRIMARY KEY,
+	nom TEXT,
+	pere INTEGER,
+	FOREIGN KEY(pere) REFERENCES categorie(id)
+);
