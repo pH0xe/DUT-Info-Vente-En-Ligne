09a72f79c97df3d434abf073dc99226c42f585fa
ElMagnificoJoris <joris.chenevas@free.fr>
2019-11-06 20:44:53 +0100

    panierUpgrade

--- view/panier.view.php
+++ view/panier.view.php
@@ -14,12 +14,15 @@
         <?php include("../view/nav.view.php"); ?>
 
         <div>
-            <?php  foreach ($_SESSION['panier']['article'] as $key => $value) { ?>
+            <?php $prixTot = 0; foreach ($_SESSION['panier']['article'] as $key => $value) { ?>
                 <div class="article">
-                    <p><?= $value->getLibelle()?> x<?= $_SESSION['panier']['quantite'][$key]?> <?php $panier->boutonSupprimer($value->getRef()); ?></p>
+                  <?php $prixTot = $value->getPrix() * $_SESSION['panier']['quantite'][$key] + $prixTot ?>
+                    <p><a class="objet" href="afficherArticle.ctrl.php?ref=<?= $value->getRef(); ?>"><img src="../data/img/<?= $value->getImage(); ?>", height="50", width="50"><?= $value->getLibelle()?></a> <?= $value->getPrix()?>€ x<?= $_SESSION['panier']['quantite'][$key]?> = <?=$value->getPrix() * $_SESSION['panier']['quantite'][$key] ?>€ <?php $panier->boutonSupprimer($value->getRef()); ?></p>
                 </div>
             <?php  } ?>
+            <p>Prix Total : <?= $prixTot?>€</p>
             <?= $panier->boutonVider(); ?>
+            <INPUT class="payer" TYPE="BUTTON" value="PAYER " ONCLICK=window.location.href='../controler/afficherPanier.ctrl.php'>
         </div>
     </div>
 </body>
